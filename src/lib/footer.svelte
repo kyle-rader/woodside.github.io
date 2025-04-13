9f997092e23bca0ddb895485dee6b6e8b7e366e6
Kyle Rader <kylewrader@gmail.com>
2025-04-13 14:24:01 -0700

    make fb group logo bigger

--- src/lib/footer.svelte
+++ src/lib/footer.svelte
@@ -6,8 +6,8 @@
 		'https://github.com/kyle-rader/woodside.github.io/actions/workflows/deploy.yml';
 </script>
 
-<footer class="mt-auto py-4">
-	<div class="container mx-auto flex items-center justify-between px-4 text-center">
+<footer class="mt-auto p-4 sm:mt-auto sm:w-80 md:w-1/2">
+	<div class="mx-auto flex justify-between">
 		<a href={deployAction} target="_blank" rel="noopener noreferrer">
 			<img src={imgUrl} alt="Deploy to GitHub Pages" />
 		</a>
